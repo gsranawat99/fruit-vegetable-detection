61750f9164beaf54ada6d8bfcbc36d0d130df8c8
Gajendrasingh Ranawat <109135827+gsranawat99@users.noreply.github.com>
2024-05-23 18:44:21 +0000

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'objectDetection',
+    version= '0.0.0',
+    author= 'gsranawat',
+    author_email= 'gsranawat99@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
